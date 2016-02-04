1825aee07fc7f24207ac9dbb3910c40657f32895
Joshua Wolfe <jlwolfe1990@gmail.com>
2016-02-03 21:06:53 -0600

    Complete class refactor

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,27 +1,10 @@
 $(document).ready( function () {
-  //handle CSS
-  $('#calendar').fullCalendar('option', 'height', $('.nav-left')[0].offsetHeight);
-
   //setup models
   var Calendar = function() {
     this.events = function(){
       return jQuery.parseJSON($("#eventsJson").html()).map(function(obj){
-        return calendar.wrapEvent(obj);
+        return new Event(obj);
       });
-    };
-    this.wrapEvent = function(event) {
-      event.allDay = true;
-      switch (event.event_type) {
-        case "Anniversary":
-          event.color = "red";
-          break;
-        case "Birthday":
-          event.color = "black";
-          break;
-        default :
-          event.color = "green";
-      }
-      return event;
     };
     this.newEvent = function(date){
       return $.ajax({
@@ -43,14 +26,34 @@ $(document).ready( function () {
         data: formData
       })
     };
+    this.resize = function(){
+      console.log("resize called");
+      return $('#calendar').fullCalendar('option', 'height', $('.nav-left').height());
+    };
   };
-  var Event = function() {
-    this.createPath = $("#eventCreatePath").html();
+
+  var Event = function(event) {
+    this.source = event;
+    var init = function(event) {
+      event.allDay = true;
+      switch (event.event_type) {
+        case "Anniversary":
+          event.color = "red";
+          break;
+        case "Birthday":
+          event.color = "black";
+          break;
+        default :
+          event.color = "green";
+      }
+      return event;
+    };
+    return init(event);
   };
 
   var calendar = new Calendar();
 
-  //setup controller
+  //setup view logic
   $('#calendar').fullCalendar({
     events: calendar.events(),
     eventRender: function (event, element) {
@@ -82,20 +85,26 @@ $(document).ready( function () {
       event.title = "CLICKED!";
 
       $('#calendar').fullCalendar('updateEvent', event);
+    },
+    windowResize: function(view){
+      calendar.resize();
     }
   });
 
-  this.submitEvent = function () {
+  //setup controller
+  document.submitEvent = function () {
     calendar.createEvent( $(".formDrop form").serialize()).done(function (event){
       if(event instanceof Object) {
         //success
         $('#newEvent').modal('toggle');
         //add colors here
-        $('#calendar').fullCalendar('renderEvent', calendar.wrapEvent(event));
+        $('#calendar').fullCalendar('renderEvent', new Event(event));
       } else {
         //invalid
         $(".formDrop").html( event );
       }
     });
   };
+
+  calendar.resize();
 });
