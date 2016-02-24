aef08c5bdfc0a5840adb2f91a28d3e5be5e23411
Joshua Wolfe <jlwolfe1990@gmail.com>
2016-02-23 21:09:35 -0600

    Delete alert requests on event destroy

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   after_update :touch_alert_requests
 
   belongs_to :user
-  has_many :alert_requests, autosave: true
+  has_many :alert_requests, autosave: true, dependent: :destroy
   accepts_nested_attributes_for :alert_requests
 
   validates :title, presence: true
